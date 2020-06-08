afe3977a0edc4788b6f19161835f71818e01fbda
Ernesto Garcia <42915429+egarciav@users.noreply.github.com>
2020-06-08 14:26:34 -0500

    Add files via upload

---
+++ javascript/iteracion-arreglos.js
@@ -0,0 +1,35 @@
+// MAP - map()
+// Este método crea un nuevo arreglo ejecutando una función 
+// en cada uno de sus elementos, sin alterar el arreglo original
+
+/*let array = [1,2,3,4,5]
+
+let newArray = array.map((item) => {
+    return item * 2;
+});
+
+alert(newArray);*/
+
+// EVERY - every()
+// Se verifica si TODOS los valores del array pasan un test/filtro
+
+
+// SOME - some()
+// Con este método podemos varificar si ALGUNO de los valores 
+// del arreglo pasan un filtro establecido
+
+
+// FILTER - filter() 
+// Al usarlos es posible crear un nuevo arreglo con elementos del original, 
+// pero que pasen un filtro o prueba
+
+
+// FIND - find()
+// Este método retorna el valor del primer elemento del array que pase 
+// un test determinado
+
+
+// FOR EACH - forEach()
+// Ejecuta código para cada elemento del arreglo
+
+
